144cb1160ffa90775f197b07f527ae898971cdad
tanvir35web <islammdtanvirul@gmail.com>
2023-12-17 22:45:44 +0600

    Hero Component added and responsive

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import './HeroStyle.css'
+import HeroImg from '../assets/intro-bg.jpg'
+
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="mask">
+            <img className='hero-img' src={HeroImg} alt="Hero Image" />
+        </div>
+        <div className="content">
+            <p>HI, I'M TANVIRUL ISLAM</p>
+            <h1>React Developer.</h1>
+            <div>
+                <Link to="/project" className='btn'> Project </Link>
+                <Link to="/contact" className='btn btn-light'> Contact </Link>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Hero;
