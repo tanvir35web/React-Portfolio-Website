22720bbb50213edeb52ca61fad93f72c0f69894a
tanvir35web <islammdtanvirul@gmail.com>
2023-12-17 20:06:20 +0600

    Nav link routed successfully

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import "./NavBarStyle.css";
+import React from "react";
+
+
+const NavBar = () => {
+  return (
+    <div className="header">
+      <Link to={"/"}>
+        <h1>Portfolio</h1>
+      </Link>
+
+      <ul>
+        <li>
+            <Link to={"/"}> Home </Link>
+        </li>
+        <li>
+            <Link to={"/project"}> Project </Link>
+        </li>
+        <li>
+            <Link to={"/about"}> About </Link>
+        </li>
+        <li>
+            <Link to={"/contact"}> Contact </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
