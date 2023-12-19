d0d0324c4b02315765b024907d5e045c05fbb3e7
tanvir35web <islammdtanvirul@gmail.com>
2023-12-19 21:52:32 +0600

    form section added on contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import HeroAll from "../components/HeroAll";
+import Form from "../components/Form";
 
 const Contact = () => {
   return (
     <>
       <NavBar />
       <HeroAll heading={"CONTACT"} text={"Hello, Let's have a chat"} />
+      <Form/>
       <Footer />
     </>
   );
