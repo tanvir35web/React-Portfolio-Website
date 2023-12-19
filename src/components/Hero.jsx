8d968916123e3555b0f419ca0f0245b134a45fb0
tanvir35web <islammdtanvirul@gmail.com>
2023-12-19 22:40:49 +0600

    Hero BG video added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
 import './HeroStyle.css'
 import HeroImg from '../assets/intro-bg.jpg'
 
+
+import myVideo from '../assets/herovideo.mp4'
+
+
 import React from 'react'
 import { Link } from 'react-router-dom';
 
@@ -8,7 +12,10 @@ const Hero = () => {
   return (
     <div className='hero'>
         <div className="mask">
-            <img className='hero-img' src={HeroImg} alt="Hero Image" />
+            {/* <img className='hero-img' src={HeroImg} alt="Hero Image" /> */}
+            <video className='hero-video' autoPlay loop muted>
+              <source src={myVideo}/>
+            </video>
         </div>
         <div className="content">
             <p>HI, I'M TANVIRUL ISLAM</p>
